79e391251eece0e933fa25193aac87e0614f08da
Caleb Sharp <epicsharp11@gmail.com>
2016-12-06 06:39:33 -0800

    Add footer to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,15 +3,18 @@ import logo from '../../logo.svg';
 import './App.css';
 
 import Header from '../Header/Header';
+import Card from '../Card/Card';
+import Footer from '../Footer/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App__content">
+          <Card />
+        </div>
+        <Footer />
       </div>
     );
   }
