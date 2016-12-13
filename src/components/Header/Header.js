390edc171e9feef104e45cb0f60a15857746bb6c
Caleb Sharp <epicsharp11@gmail.com>
2016-12-12 22:10:29 -0800

    Better words

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
       <div className="Header">
         <div className="Header__content">
           <h1 className="Header__title">Caleb Sharp</h1>
-          <p className="Header__subtitle">Programmer, thinker, and all-around Potato&trade;</p>
+          <p className="Header__subtitle">Programmer, thinker, and self-certified Potato&trade;</p>
         </div>
       </div>
     );
